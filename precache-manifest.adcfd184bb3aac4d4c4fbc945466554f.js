ce8861d11e953d83ade62d081f3efa586b57a7aa
Rajesh saha <rajeshsaha782@gmail.com>
2020-10-15 13:28:16 +0600

    Updates

---
+++ precache-manifest.adcfd184bb3aac4d4c4fbc945466554f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f35a8534eb3a2d81abb97c5423aeba",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "5dec57d1127243acaffa",
+    "url": "/todo/static/css/main.a99200be.chunk.css"
+  },
+  {
+    "revision": "07e907d27fec7f7263a7",
+    "url": "/todo/static/js/2.8b099061.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.8b099061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dec57d1127243acaffa",
+    "url": "/todo/static/js/main.f5248b6f.chunk.js"
+  },
+  {
+    "revision": "68c7d85bcc981c9d2308",
+    "url": "/todo/static/js/runtime-main.0704c77e.js"
+  }
+]);
